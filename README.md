3fdbeb8f7d35f871d2c90a466ca20048b7951c96
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-07 11:51:28 -0300

    MY05 - Exibindo dados do Banco de Dados com EJS Embedded JavaScript templates

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # RestauranteSaboroso
+
+sudo npm install express-generator -g
+sudo npm install -g bower
+cd public/admin/
+bower install
+npm install -g nodemon
+set DEBUG=saboroso:* & npm start
+
+para criação do banco de dados, basta utilizar a migration que está no caminho public/db/mysql.sql
+
+
+-- necessario ter 
+nodejs
+mysql
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
+var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  conn.query(`
+    SELECT * FROM tb_menus ORDER BY title
+  `, (err, results) => {
+
+    if(err){
+      console.error(err);
+    }
+    
+  res.render('index', { 
+    title: 'Restaurante Saboroso!',
+    menus: results
+  });
+    
+
+  });
 });
 
 module.exports = router;
