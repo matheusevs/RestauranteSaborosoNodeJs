f42c975d6d82114b9082ee28652e8683b2ee6487
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-09 14:45:16 -0300

    MY06 - EJS Include

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,36 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/contacts', function(req, res, next){
+
+  res.render('contacts', {
+    title: 'Contatos - Restaurante Saboroso!'
+  });
+
+});
+
+router.get('/menus', function(req, res, next){
+
+  res.render('menus', {
+    title: 'Menus - Restaurante Saboroso!'
+  });
+
+});
+
+router.get('/reservations', function(req, res, next){
+
+  res.render('reservations', {
+    title: 'Reservas - Restaurante Saboroso!'
+  });
+
+});
+
+router.get('/services', function(req, res, next){
+
+  res.render('services', {
+    title: 'Serviços - Restaurante Saboroso!'
+  });
+
+});
+
 module.exports = router;
