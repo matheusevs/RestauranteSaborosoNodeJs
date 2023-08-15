1e82c667bb827706baec3a32c986a6a0fdfbe6c3
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-15 14:41:03 -0300

    MY23 - Administração do Menu - Criando Novo Menu

--- routes/admin.js
+++ routes/admin.js
@@ -108,6 +108,12 @@ router.get('/menus', function(req, res, next){
 
 });
 
+router.post('/menus', function(req, res, next){
+
+    res.send(req.body);
+
+});
+
 router.get('/reservations', function(req, res, next){
 
     res.render('admin/reservations', admin.getParams(req));
