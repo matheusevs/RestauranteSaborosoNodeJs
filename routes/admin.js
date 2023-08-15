2f1703380cae182b52a0a14bdb615d430e23ef65
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-15 14:28:33 -0300

    MY22 - Administração do Menu - Consultando dados no MySQL

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var users = require('./../inc/users');
 var admin = require('./../inc/admin');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -93,7 +94,17 @@ router.get('/emails', function(req, res, next){
 
 router.get('/menus', function(req, res, next){
 
-    res.render('admin/menus', admin.getParams(req));
+    menus.getMenus().then(data => {
+        
+        res.render('admin/menus', admin.getParams(req, {
+            data
+        }));
+
+    }).catch(error => {
+
+        console.error(error);
+
+    });
 
 });
 
