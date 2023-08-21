0d101b778aeb4614f43dd0f14c54530b68ee1fc7
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-21 12:14:18 -0300

    MY35 - Criando componente Grid para tabela

---
+++ public/js/grid.js
@@ -0,0 +1,123 @@
+class Grid{
+
+    constructor(configs){
+
+        this.options = Object.assign({}, {
+            formCreate: '#modal-create form',
+            formUpdate: '#modal-update form',
+            btnUpdate: '.btn-update',
+            btnDelete: '.btn-delete',
+        }, configs);
+
+        this.initForms();
+        this.initButtons();
+
+    }
+
+    initForms(){
+
+        this.formCreate = document.querySelector(this.options.formCreate);
+
+        this.formCreate.save().then(json => {
+
+            window.location.reload();
+
+        }).catch(error => {
+
+            console.error(error);
+
+        });
+
+        this.formUpdate = document.querySelector(this.options.formUpdate);
+
+        this.formUpdate.save().then(json => {
+
+            window.location.reload();
+
+        }).catch(error => {
+
+            console.error(error);
+
+        });
+
+    }
+
+    initButtons(){
+
+        [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
+
+            btn.addEventListener('click', e => {
+
+                let tr = e.path.find(el => {
+
+                    return (el.tagName.toUpperCase() === 'TR');
+
+                });
+
+                let data = JSON.parse(tr.dataset.row);
+
+                for(let name in data){
+
+                    let input = this.formUpdate.querySelector(`[name=${name}]`);
+
+                    switch(name){
+
+                        case 'date':
+                            if(input){
+
+                                input.value = moment(data[name]).format('YYYY-MM-DD');
+                            
+                            }
+                        break;
+
+                        default:                    
+                            if(input){
+
+                                input.value = data[name];
+                            
+                            } 
+
+                    }
+
+                }
+
+                $('#modal-update').modal('show');
+
+            });
+
+        });
+
+
+        [...document.querySelectorAll(this.options.btnDelete)].forEach(btn => {
+
+            btn.addEventListener('click', e => {
+
+                let tr = e.path.find(el => {
+
+                    return (el.tagName.toUpperCase() === 'TR');
+
+                });
+
+                let data = JSON.parse(tr.dataset.row);
+
+                if(confirm(eval('`' + this.options.deleteMsg + '`'))){
+
+                    fetch(eval('`' + this.options.deleteUrl + '`'), {
+                        method: 'DELETE'
+                    })
+                    .then(response => response.json())
+                    .then(json => {
+            
+                        window.location.reload();
+            
+                    });
+                
+                }
+
+            });
+
+        });
+        
+    }
+
+}
