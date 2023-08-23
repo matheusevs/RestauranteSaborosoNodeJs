7fd3f67adc8dc50cd8a341d21dafd285118eef6c
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-23 14:32:13 -0300

    MY46 - Trabalhando com paginação de dados, botões anterior e próxima

--- inc/Pagination.js
+++ inc/Pagination.js
@@ -90,12 +90,30 @@ class Pagination {
 
         }
 
+        if(this.getCurrentPage() > 1){
+
+            links.push({
+                text: '«',
+                href: '?' + this.getQueryString(Object.assign({}, params, {page: this.getCurrentPage() - 1}))
+            });
+
+        }
+
         for(let x = nrStart; x <= nrEnd; x++){
 
             links.push({
                 text: x,
                 href: '?' + this.getQueryString(Object.assign({}, params, {page: x})),
                 active: (x === this.getCurrentPage())
+            });
+
+        }
+
+        if(this.getCurrentPage() < this.getTotalPages()){
+
+            links.push({
+                text: '»',
+                href: '?' + this.getQueryString(Object.assign({}, params, {page: this.getCurrentPage() + 1}))
             });
 
         }
